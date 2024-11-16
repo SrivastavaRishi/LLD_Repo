1afaf59e1e878c38d961c074d472ef9b123507d1
Rishi-Srivastava <srishi0310@gmail.com>
2024-11-16 16:03:27 +0530

    singleton class

--- src/CreationalDesignPattern/Singleton/Main.java
+++ src/CreationalDesignPattern/Singleton/Main.java
@@ -1,5 +1,7 @@
 package CreationalDesignPattern.Singleton;
 
+import java.util.ArrayList;
+import java.util.List;
 
 class SingletonClass{
     int a;
@@ -8,6 +10,44 @@ class SingletonClass{
     }
     public void print(){
         System.out.println("Value of a is " + a);
+    }
+}
+
+class Connection{
+    String connectionString;
+}
+
+class DatabaseConnectionManager {
+    private static final int MAX_CONNECTIONS = 10;
+    private static DatabaseConnectionManager instance;
+    private List<Connection> connectionPool;
+
+    private DatabaseConnectionManager() {
+        connectionPool = new ArrayList<>(MAX_CONNECTIONS);
+        for (int i = 0; i < MAX_CONNECTIONS; i++) {
+            connectionPool.add(createConnection());
+        }
+    }
+
+    private Connection createConnection() {
+        // Create and return a new database connection
+        return connectionPool.getFirst();
+    }
+
+    synchronized public static DatabaseConnectionManager getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnectionManager();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        // Return a connection from the pool
+        return connectionPool.getFirst();
+    }
+
+    public void releaseConnection(Connection connection) {
+        // Release the connection back to the pool
     }
 }
 
--- src/CreationalDesignPattern/Singleton/readme.md
+++ src/CreationalDesignPattern/Singleton/readme.md
@@ -1,1 +1,7 @@
 Singleton class
+    - Constructor is private
+
+1. Eager Initialisation
+2. Lazy Initiazation
+3. Locking
+4. Double Locking
